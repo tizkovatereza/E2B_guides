c566549d03b077279f007bc556c1e2cd59d283af
Tereza Tizkova <tereza@e2b.dev>
2023-10-17 15:02:10 -0700

    Clean code

--- Data_analyst_v0.1.py
+++ Data_analyst_v0.1.py
@@ -1,26 +1,9 @@
-# pip install e2b
-# pip install openai
-
-# Latest version
-# pip install e2b --upgrade
-
-
-# Import everything and start the E2B session
-
 import e2b
-
-
 import openai
 from dotenv import load_dotenv
 
 load_dotenv()
-
-
-# Create session variable
 session = e2b.DataAnalysis(env_vars={})
-
-
-# Upload and download a local file
 
 with open("/Users/terezatizkova/Downloads/Air_Quality.csv", "rb") as f:
     remote_path = session.upload_file(file=f)
@@ -29,24 +12,15 @@ file = session.download_file(remote_path)
 
 downloaded_file_in_bytes = session.download_file(remote_path)
 with open("/Users/terezatizkova/Downloads/Air_Quality_Chart.csv", "wb") as f:
-  f.write(downloaded_file_in_bytes)   # effectively saving the content of the downloaded file to the chosen file on your local machine
-
-  #wb ... opened for writing, "b" indicates that you will work with the file's content as a sequence of binary bytes rather than as text
-
+  f.write(downloaded_file_in_bytes)  
 
 # Install packages
 
 #session.install_packages("name") - THIS IS JUST GENERIC?
 #session.install_system_packages("name")
 
-
-# Save the generated charts
-
 def handle_new_artifact(artifact):
     chart_file = artifact.download()
-
-
-# Run the session
 
 session.run_python(
     code="print(\"valid code\")",
@@ -54,9 +28,3 @@ session.run_python(
     on_stderr=print,
     on_artifact=handle_new_artifact,
 )
-
-
-
-
-
-
